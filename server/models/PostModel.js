c5094628ba8e6b6371c9c4a967bfc77dd7036c29
MinhChuoi <ngominh99it1@gmail.com>
2021-07-14 16:11:15 +0700

    first commit

---
+++ server/models/PostModel.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    // require: true
+  },
+  content: {
+    type: String,
+    require: true
+  },
+  author: {
+    type: String,
+    require: true,
+    default: 'Anonymous'
+  },
+  attactment: String,
+  likeCount: {
+    type: Number,
+    default: 0
+  },
+}, { timestamp: true });
+export const PostModel = mongoose.model('Post', schema)
